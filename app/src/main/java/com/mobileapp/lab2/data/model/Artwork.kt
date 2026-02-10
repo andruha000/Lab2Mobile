c63fdb0ded1f447645619e8cc7904dc6f55e7f8e
Andrey Novikov <andreyka0505@gmail.com>
2026-02-10 21:48:41 +0400

    feat: реализовано приложение для просмотра произведений искусства

---
+++ app/src/main/java/com/mobileapp/lab2/data/model/Artwork.kt
@@ -0,0 +1,8 @@
+package com.mobileapp.lab2.data.model
+
+data class Artwork(
+    val imageId: Int,
+    val titleId: Int,
+    val artistId: Int,
+    val descriptionId: Int
+)
--- app/src/main/java/com/mobileapp/lab2/ui/theme/Color.kt
+++ app/src/main/java/com/mobileapp/lab2/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.mobileapp.lab2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green = Color(0xFF009157)
+val Gray = Color(0xFFD0D0D0)
+val White = Color.White
+val Black = Color.Black
+val LightGray = Color(0xFFE3E3E3)
+val DarkGray = Color(0xFF0A0A0A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkGray2 = Color(0xFF1E1E1E)
+val LightGreen = Color(0x27009157)
